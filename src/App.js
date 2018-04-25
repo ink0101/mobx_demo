a5120986091c9c09816f0680a180cc0151a2b45c
ink0101 <cftt2016@gmail.com>
2018-04-25 16:29:15 +0800

    [add] 添加路由demo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom'
+
 import './App.css';
+import AppRouter from './route/AppRouter.jsx';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Router>
+          <div>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/home">Home</Link>
+                </li>
+                <li>
+                  <Link to="/demo1">demo1</Link>
+                </li>
+              </ul>
+            </nav>
+            <div>
+              <Route exact={true} strict={true} path="/home" component={Home} />
+              <Route exact={true} strict={true} path="/demo1" component={Demo1} />
+            </div>
+          </div>
+        </Router>
       </div>
     );
   }
 }
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+const Demo1 = () => (
+  <div>
+    <h2>Demo1</h2>
+  </div>
+)
 
 export default App;
