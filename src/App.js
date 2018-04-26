2226970375822587d9016d3c70b22383eb360510
ink0101 <cftt2016@gmail.com>
2018-04-26 13:41:24 +0800

    [fix] 调整路由

--- src/App.js
+++ src/App.js
@@ -7,44 +7,11 @@ import DemoRouter from './route/DemoRouter.jsx';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Router>
-          <div>
-            <nav>
-              <ul>
-                <li>
-                  <Link to="/home">Home</Link>
-                </li>
-                <li>
-                  <Link to="/demo1">demo1</Link>
-                </li>
-                <li>
-                  <Link to="/demo2">demo2</Link>
-                </li>
-              </ul>
-            </nav>
-            <div>
-              <Route exact={true} strict={true} path="/home" component={Home} />
-              <Route exact={true} strict={true} path="/demo1" component={Demo1} />
-              <DemoRouter {...this.props} />
-            </div>
-          </div>
-        </Router>
-      </div>
+      <Router>
+        <DemoRouter {...this.props} />
+      </Router>
     );
   }
 }
-
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-)
-
-const Demo1 = () => (
-  <div>
-    <h2>Demo1</h2>
-  </div>
-)
 
 export default App;
--- src/route/DemoRouter.jsx
+++ src/route/DemoRouter.jsx
@@ -1,14 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { HashRouter as Router, Link, Route } from 'react-router-dom'
 
+import Demo1 from '../views/Demo1';
 import Demo2 from '../views/Demo2';
 
 class DemoRouter extends Component {
   render() {
     return (
-      <Router>
+      <Fragment>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/demo1">demo1</Link>
+            </li>
+            <li>
+              <Link to="/demo2">demo2</Link>
+            </li>
+          </ul>
+        </nav>
+        <Route exact={true} strict={true} path="/demo1" component={Demo1} />
         <Route exact={true} strict={true} path="/demo2" component={Demo2} />
-      </Router>
+      </Fragment>
+      
     );
   }
 }
