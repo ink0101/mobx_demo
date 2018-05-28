57678857a4c850ac46ab6799fd78a762fe477de8
ink0101 <cftt2016@gmail.com>
2018-05-28 12:03:31 +0800

    [fix] 计算两个数字累加和

--- src/mobx/State.js
+++ src/mobx/State.js
@@ -1,9 +1,19 @@
-import { observable, action } from "mobx";
+import { observable, action, computed } from "mobx";
 
 class State {
-  @observable num = 0;
-  @action addNum = () => {
-    return this.num ++;
+  @observable num1 = 0;
+  @action addNum1 = () => {
+    return this.num1 ++;
+  }
+
+  @observable num2 = 0;
+  @action addNum2 = () => {
+    return this.num2 ++;
+  }
+
+  @computed
+  get total () {
+    return this.num1 + this.num2;
   }
 }
 
--- src/views/Demo2.jsx
+++ src/views/Demo2.jsx
@@ -11,8 +11,13 @@ class Demo2 extends Component {
   render() {
     return (
       <div>
-        <p>{newState.num}</p>
-        <button type="button" onClick={newState.addNum}> + 1 </button>
+        <p>num1: {newState.num1}</p>
+        <button type="button" onClick={newState.addNum1}> num1 + 1 </button>
+
+        <p>num2: {newState.num2}</p>
+        <button type="button" onClick={newState.addNum2}> num2 + 1 </button>
+
+        <p>total: {newState.total}</p>
       </div>
     );
   }
